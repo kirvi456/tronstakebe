8563670f17eb77c54abaca5f17c9159ab9bb618f
kirvi456 <kirvi16@gmail.com>
2020-10-13 15:57:18 -0600

    Revert "firts commit"
    This reverts commit 0f53b4d603470c9bc2a582ec10177ee60a91bb8e.

--- routes/valor-moneda.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var express = require("express");
-var controller = require("../controllers/valor-moneda");
-var api = express.Router();
-
-api.post("/getPorHora", controller.getPorHora);
-api.post("/postValor", controller.postValor);
-
-module.exports = api;
