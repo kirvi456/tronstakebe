abb70ebcec8a4888fe9855e96aa1036876e5795d
kirvi456 <kirvi16@gmail.com>
2020-10-19 11:10:19 -0600

    firts commit

--- controllers/valorMoneda.js
+++ controllers/valorMoneda.js
@@ -11,7 +11,7 @@ exports.getPorHora = function (req, res) {
 };
 
 exports.getDatos = function (req, res) {
-  valorMoneda.find({}, function(err, resultado){
+  valorMoneda.find({}).sort('-fecha').limit(1).exec(function(err, resultado){
       if(err) return res.status(500).send("No se pudo conectar a la base de datos.");
       if(!resultado) res.status(200).json('[]');
       res.status(200).json(resultado);
