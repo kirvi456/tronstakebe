2376a55d5ec3c75c0e0734436d6d5fa770b20b47
kirvi456 <maritojones@hotmail.com>
2020-11-02 20:43:58 -0600

    cosa para la reinversiones

--- controllers/retiroController.js
+++ controllers/retiroController.js
@@ -24,8 +24,8 @@ exports.postValor = function (req, res) {
     var _player = req.body.player;
 
     retiroModel.findOne({player:_player}, function(error, result) {
-        if(err) return res.status(200).json('{valor: 0}');
-        if(!resultado) res.status(200).json('{valor: 0}');
-        res.status(200).json(resultado);
+        if(error) return res.status(200).json('{valor: 0}');
+        if(!result) res.status(200).json('{valor: 0}');
+        res.status(200).json(result);
     });        
   };
